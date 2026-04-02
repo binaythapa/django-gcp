c1887d06ac950362b03afc50bb591506a356415e
binay <tbinay5@gmail.com>
2026-04-02 23:15:15 +0545

    updated repo

---
+++ hello_project/settings.py
@@ -0,0 +1,25 @@
+SECRET_KEY = 'dummy-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = 'hello_project.urls'
+
+TEMPLATES = []
+
+WSGI_APPLICATION = 'hello_project.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
